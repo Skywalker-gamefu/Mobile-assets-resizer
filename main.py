2c0a97e56303fde16cbe6304815ecacab7375e2e
skywalker <skywalkerprog@gmail.com>
2016-11-30 14:09:20 +0200

    creating empty app files and folders

---
+++ main.py
@@ -0,0 +1,13 @@
+# this script use to resize mobile apllication images assets.abs
+# developed by skywalker using pillow moudel.
+# contact 
+
+#---------------------------------------------------------------
+
+# using pip to install pillow
+#import setup
+
+
+#main function
+if __name__ == '__main__':
+  
