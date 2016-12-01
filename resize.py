be1656eef9cb366823d9c049bb43a191d6b165b0
skywalker <skywalkerprog@gmail.com>
2016-12-01 16:18:17 +0200

    removing getInfo func make all vars global

--- resize.py
+++ resize.py
@@ -1,1 +1,17 @@
 #resize.py moudel
+import os
+
+srcImgList = os.listdir("source") #get list for all files in source directoryne
+resultDirs = ["result/XXLarge/" , "result/XLarge/" , "result/Large/" , "result/Normal/" , "result/Small/"]#folder to save resized images
+scalRatio = [1 , 960/1440 , 720/1440 , 480/1440 , 360/1440]
+size = [50 , 50] #image size [width and height]
+supportedFormat = ["png" , "jpeg" , "jpg"]
+print("start resizing ...")
+
+def resizeNow():
+
+    scalIndexer = 0
+
+
+getInfo()
+resizeNow()
